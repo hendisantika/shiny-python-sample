2bd30cd9ef7ec6c7be032fc4725f6ac23a2d9c67
hendisantika <hendisantika@yahoo.co.id>
2025-03-23 07:54:29 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ app.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import pandas
+import pandas as pd
+import plotly.express as px
+from shiny import reactive
+from shiny.express import render, input, ui
+from shinywidgets import render_plotly
+
+ui.page_opts(title="Sales Dashboard", fillable=True)
+
+ui.input_numeric("num", label="Enter a number of Items", value=5, min=0, max=20)
+
+
+@reactive.calc
+def dat():
+    infile = Path(__file__).parent / "data/sales.csv"
+    return pd.read_csv(infile)
+
+
+with ui.layout_columns():
+    # @render_plotly
+    # def plot1():
+    #     df = dat()
+    #     return px.histogram(px.data.tips(), y="tip")
+
+    # @render_plotly
+    # def plot2():
+    #     return px.histogram(px.data.tips(), y="total_bill")
+
+    @render_plotly
+    def plot3():
+        df = dat()
+        top_sales = df.groupby('product')['quantity_ordered'].sum().nlargest(input.num()).reset_index()
+        return px.bar(top_sales, x='product', y='quantity_ordered')
+
+    # @render.data_frame
+    # def data():
+    #     return dat()
